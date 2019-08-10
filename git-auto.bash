64e76c394eebb920e6ebd7a8b53db5c3f36a012f
Huynh Minh Tan <minhtan.itdev@gmail.com>
2019-08-10 18:41:44 +0700

    update onehit

--- git-auto.bash
+++ git-auto.bash
@@ -6,9 +6,15 @@ git push origin mt-theme-v1
 # cd gh-pages
 cd ../gh-pages/
 git checkout gh-pages
-if [ -d ".git" ]; then
+if [ -d ".git" ]; 
+then
     git rm -rf .
     git clean -fxd
+else
+    git init
+    git remote add origin https://github.com/huynhminhtan/mtsinichi.github.io.git
+    git config user.name "Huynh Minh Tan"
+    git config user.email "minhtan.itdev@gmail.com"
 fi
 
 # copy to gh-pages
